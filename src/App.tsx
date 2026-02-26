fa8660c860d20968b033b2d378207cef03d7ee91
Oleg Shalyakin <Aligattor@mail.ru>
2026-02-27 01:06:30 +0500

    little refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,27 @@ import "./App.css";
 import Header from "./components/Header";
 import MainBoard from "./components/MainBoard";
 import MenuRight from "./components/MenuRight";
+import LoginPage from "./components/login-page/LoginPage.tsx";
+
+const LOGIN_ATTRIBUTE = "isLoggedIn";
 
 function App() {
-  return (
-    <div className="app-container">
-      <header>
-        <Header />
-      </header>
-      <main>
-        <MainBoard />
-        <MenuRight />
-      </main>
-    </div>
-  );
+
+    if (isLoggedIn()) {
+        return (
+            <div className="app-container">
+                <Header/>
+                <MainBoard/>
+                <MenuRight/>
+            </div>
+        );
+    }
+
+    return <LoginPage/>
+}
+
+function isLoggedIn(): boolean {
+    return localStorage.getItem(LOGIN_ATTRIBUTE) as unknown as boolean | false;
 }
 
 export default App;
