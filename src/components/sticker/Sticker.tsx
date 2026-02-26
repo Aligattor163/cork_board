d221d1216246cdb42c39671c42692ca3f9cdb2e5
Oleg Shalyakin <Aligattor@mail.ru>
2026-02-27 01:08:21 +0500

    Sticker component implementation

---
+++ src/components/sticker/Sticker.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './sticker.css'
+
+const Sticker: React.FC = () => {
+    return (
+        <div className="sticker">
+            <div className="sticker__pin"></div>
+            <div className="sticker__body">
+                <span className="sticker__header-text">Note #1</span>
+                <span className="sticker__content-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, illum!</span>
+            </div>
+
+        </div>
+    )
+}
+
+export default Sticker
