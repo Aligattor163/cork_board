48aa6572e2ae87d321c0983040ef16f620ac13c5
Oleg Shalyakin <Aligattor@mail.ru>
2026-03-01 04:30:06 +0500

    debbuging logger impl

---
+++ src/services/log-service.tsx
@@ -0,0 +1,9 @@
+const DEBUG_STORAGE_PROPERTY_NAME: string = "isDebugModeOn";
+const LOGGER_MESSAGE_TEMPLATE: string = "[CorkBoard_app_DEBUG]: <%s>"
+export const Logger = {
+    debug: (message: string) => isDebug() && console.debug(LOGGER_MESSAGE_TEMPLATE.replace("%s", message))
+}
+
+function isDebug(): boolean {
+    return localStorage.getItem(DEBUG_STORAGE_PROPERTY_NAME) === "true";
+}
